02225f084a892a307cc877a63cebec1e6c92798f
Florian Schaefer <flotosch@gmail.com>
2023-04-23 20:30:07 -0400

    removed deprecated test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,6 @@ using Test
 
 @testset "KoLesky.jl" begin
     # Write your tests here.
-    @testset "Points.jl" begin
-        include("test_Points.jl")
-    end
     @testset "MaximinNN.jl" begin
         include("test_MaximinNN.jl")
     end
